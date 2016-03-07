46a30c9091ac256eaa5d305733f3fdd67497e9ab
Bobby Fatemi <bfatemi07@gmail.com>
2016-03-06 20:28:43 -0500

    new install files added in repoInstall

---
+++ repoInstall/installdeployr.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+##################################################################
+# Adding dedicated R group and user
+##################################################################
+
+deployrUser=deployr-user
+deployrPassword=Newyork1
+deployrGroup=deployr-group
+
+sudo addgroup $deployrGroup
+echo -e "$deployrPassword\n$deployrPassword" | \
+sudo adduser --ingroup $deployrGroup $deployrUser
+sudo adduser $deployrUser sudo
+
+##################################################################
+# Installing DeployR
+##################################################################
+
+deployR="https://github.com/bfatemi/DeployR/releases/download/v0.2/NQ_deployR-8.0.0_ubuntu14.04.tar.gz"
+dl_dir="/home/deployr-user/deployrdownload"
+
+pushd "/home/deployr-user"
+wget $deployR
+mkdir "deployrdownload"
+tar zxvf "NQ_deployR-8.0.0_ubuntu14.04.tar.gz"
+
+#pushd installFiles
+#sudo /installDeployROpen.sh --no-ask --nolicense
+#popd
+#popd
+
+#update tomcat7.sh to export JAVA_HOME
+#update properties.sh to export JAVA_HOME
+
+#these programs should not export JAVA_HOME. there should be a link to the latest and these should export that. If I update java in the future, these will export the wrong java anytime I start/stop the server
+
+#investigate why process takes so long to bind to 8006 and 8002
+#"" why groovy looks the way it does
+
+
+#after installation, run this:
+#sudo netstat -lpn |grep :80
+
---
+++ repoInstall/installdeps.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+install(){
+        sudo apt-get --yes install $1
+}
+
+########################################################################
+# Build Essentials
+########################################################################
+
+apt-get --yes update
+apt-get --yes upgrade
+apt-get --yes clean
+
+install curl
+install build-essential
+install liblzma-dev
+install libcurl4-openssl-dev
+install libpcre3-dev
+install liblzma-dev
+install zlib1g-dev
+
+# Install Dependancies
+install make
+install gcc
+install gfortran
+install nfs-common
+#install default-jdk
+
+apt-get --yes upgrade
+apt-get --yes clean
+
+
+########################################################################
+# Install JAVA
+########################################################################
+
+jdir="/usr/lib/jvm"
+mkdir $jdir
+pushd $jdir
+wget "https://github.com/bfatemi/DeployR/releases/download/v0.2/jre-8u73-linux-x64.gz"
+tar zxvf jre-8u73-linux-x64.gz
+popd
+
+JAVA_HOME="$jdir/jre1.8.0_73"
+JAVA_PATH="$JAVA_HOME/bin/java"
+printf "%s\n" "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
+source ~/.bashrc
+
+update-alternatives --install /usr/bin/java java $JAVA_PATH 100
+
+########################################################################
+# Install RRO
+########################################################################
+
+RRO_322="https://github.com/bfatemi/DeployR/releases/download/v0.2/RRO-3.2.2-Ubuntu-14.4.x86_64.deb"
+
+wget $RRO_322
+dpkg -i RRO-3.2.2-Ubuntu-14.4.x86_64.deb > /dev/null 2>&1
+if [ $? -gt 0 ]; then
+        apt-get -f --force-yes --yes install > /dev/null 2>&1
+fi
+dpkg -i RRO-3.2.2-Ubuntu-14.4.x86_64.deb > /dev/null 2>&1
+
+########################################################################
+# Install MLK
+########################################################################
+
+MLK_322="https://github.com/bfatemi/DeployR/releases/download/v0.2/RevoMath-3.2.2.tar.gz"
+wget $MLK_322
+tar zxvf RevoMath-3.2.2.tar.gz
+
+pushd RevoMath/
+sudo ./RevoMath.sh
+popd
+
+########################################################################
+# Install DeployrRserve
+########################################################################
+
+deployrRserve="https://github.com/deployr/deployr-rserve/releases/download/v7.4.2/deployrRserve_7.4.2.tar.gz"
+wget $deployrRserve
+R CMD INSTALL deployrRserve_7.4.2.tar.gz
+
+#run R
+clear
+R
+exit 0
+
